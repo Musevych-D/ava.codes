7fcb28e7307586d583dba50c7185dade5d6a4137
Denis Musevych <avobacha@gmail.com>
2018-12-15 12:20:20 +0200

    ready for code review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {ShopModule} from './components/shop/shop.module';
+import {HeaderModule} from './components/header/header.module';
+import {CartModule} from './components/header/cart/cart.module';
+
+import {CartService} from './components/header/cart/service/cart.service';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {routing} from './app.routing';
+import { GreetingComponent } from './components/greeting/greeting.component';
+import {ShopService} from './shared-services/shop.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    GreetingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    routing,
+    ShopModule,
+    HeaderModule,
+    CartModule,
   ],
-  providers: [],
+  providers: [CartService, ShopService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
